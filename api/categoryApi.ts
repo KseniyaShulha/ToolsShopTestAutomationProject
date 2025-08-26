10e0389fc303110694c4e5d1033f0485ac00e840
KseniyaShulha <kseniyashulha90@gmail.com>
2025-08-26 17:09:35 +0200

    TOOLS-34 GET request to /api/products with query parameter category (#28)

---
+++ api/categoryApi.ts
@@ -0,0 +1,35 @@
+import { APIRequestContext, expect, Request } from "@playwright/test";
+import BaseAPI from "./baseAPI";
+import { responseData } from "./requestData/productsData";
+
+export class CategoryApi extends BaseAPI {
+  protected path: string;
+  responseData: any;
+
+  // Constructor for the class
+  constructor(request: APIRequestContext) {
+    super(request);
+
+    // Define common path for each req of class
+    this.path = "category";
+    this.responseData = responseData;
+  }
+
+  async getProducts(params: string = "?page=1", request: any = this.request) {
+    const url = this.apiUrl + this.path + params;
+
+    console.log(`Send GET ${url}`);
+
+    // Send get request to get products list
+    const response = await request.get(url, {
+      headers: this.headersObj,
+    });
+
+    console.log(
+      `Response ${url}: `,
+      JSON.stringify(await response.json(), null, 2),
+    );
+
+    return response;
+  }
+}
