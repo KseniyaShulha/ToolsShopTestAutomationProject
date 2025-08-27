79d12ad59743bef1b842fabd184ca886416dd4f9
KseniyaShulha <kseniyashulha90@gmail.com>
2025-08-27 18:23:56 +0200

    TOOLS-35 GET request to /api/products with query parameter search

--- tests/api/TOOLS_35_getProductsWithQueryParameterSearch.spec.ts
+++ tests/api/TOOLS_35_getProductsWithQueryParameterSearch.spec.ts
@@ -12,39 +12,50 @@ test.describe("GET/products with query parameter search", async () => {
     const getAllProductsResponse = await productsApi.getProducts(
       "?page=1&between=price,0,200",
     );
-
     const responseBody = await getAllProductsResponse.json();
 
     product = responseBody.data.find((el) => el.name.includes(" "));
     console.log("Product to test search: ", JSON.stringify(product, null, 2));
   });
 
-  test("TOOLS-35 GET products/search?q=", async ({ request }) => {
-    // Send GET request with query parameter search and save response in var
-    const getProductsBySearchResponse = await productsApi.getProducts(
-      `/search?q=${encodeURIComponent(product.name)}`,
-    );
+    test("TOOLS-35 GET products/search?q=", async ({ request }) => {
+      // Send GET request with query parameter search and save response in var
+      const getProductsBySearchResponse = await productsApi.getProducts(
+        `/search?q=${encodeURIComponent(product.name)}`,
+      );
 
-    // Assert response status is equal to 2**
-    await expect(getProductsBySearchResponse).toBeOK();
+      // Assert response status is equal to 2**
+      await expect(getProductsBySearchResponse).toBeOK();
 
-    //Save response body in json in var
-    const responseBody = await getProductsBySearchResponse.json();
+      //Save response body in json in var
+      const responseBody = await getProductsBySearchResponse.json();
 
-    let nameArr = product.name.split(" ");
-    let firstWord = nameArr[0];
-    let secondWord = nameArr[1];
+      const searchCombinations = [
+        product.name,
+        product.name.split(" ")[0],
+        product.name.split(" ")[1],
+      ];
 
-    // Itterate over response body data to check the price to be less or equal 5
-    for (const key of responseBody.data) {
-      // Assert full names of products from response body to be full product name
-      expect.soft(key.name).toBe(product.name);
+      for (const q of searchCombinations) {
+        console.log(`Searching by q: ${q}`);
 
-      // Assert full names of products from response body to to contain first word from product name
-      expect.soft(key.name).toContain(firstWord);
+        const getProductsBySearchResponse = await productsApi.getProducts(
+          `/search?q=${encodeURIComponent(q)}`,
+        );
 
-      // Assert full names of products from response body to to contain second word from product name
-      expect.soft(key.name).toContain(secondWord);
-    }
+        // Assert response status is equal to 2**
+        await expect(getProductsBySearchResponse).toBeOK();
+
+        //Save response body in json in var
+        const responseBody = await getProductsBySearchResponse.json();
+
+        // Itterate over response body data to check the price to be less or equal 5
+        for (const product of responseBody.data) {
+          // Assert full names of products from response body to be full product name
+          expect.soft(product.name).toContain(q);
+        }
+        console.log("\n\n==============");
+      }
+    });
   });
-});
+
