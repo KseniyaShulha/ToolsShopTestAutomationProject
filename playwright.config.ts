f0011c27df20dd319e51781f0f97082f91cb15e2
KseniyaShulha <kseniyashulha90@gmail.com>
2025-11-18 13:28:31 +0100

    TOOLS-55 Workflow (#45)
    * TOOLS-55 Workflow
    
    * Add npm install
    
    * Testing workflow
    
    * Change tag to smoke
    
    * Add process.env.CI
    
    * Change a tag to ci
    
    * add reports
    
    * Add slack section into workflow
    
    * Fix environment
    
    * Fix slack section in workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix blocks in slack section of workflow
    
    * Fix yaml
    
    * Fixed configuration for sending reports to slack
    
    * Fixed commit configuration for sending reports to slack
    
    * Fixed commit configuration for sending reports to slack
    
    * Fixed commit configuration for sending reports to slack
    
    * Added environment variables in workflow to prevent script injection attacks

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 
   retries: process.env.CI ? 2 : 0,
 
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 5 : 1,
 
   reporter: [["html", { open: "never" }]],
 
