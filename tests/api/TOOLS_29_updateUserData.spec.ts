8fb932fd3a96f79f7d62b984e87dcabecf59f3f5
KseniyaShulha <kseniyashulha90@gmail.com>
2026-09-24 19:26:08 +0200

    TOOLS-75 Use structuredClone instead of shallow copy in TOOLS_29 (#64)

--- tests/api/TOOLS_29_updateUserData.spec.ts
+++ tests/api/TOOLS_29_updateUserData.spec.ts
@@ -6,7 +6,7 @@ import { signUpApi, loginApi } from "../../api/apiHelper";
 
 let token: any;
 let userID: any;
-let clonedTestDataObj: any = { ...testData_TOOLS_29.body };
+let clonedTestDataObj: any = structuredClone(testData_TOOLS_29.body);
 
 test.describe(
   "UpdateUserData",
