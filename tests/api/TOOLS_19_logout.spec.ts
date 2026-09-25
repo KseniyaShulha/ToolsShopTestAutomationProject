6b280622d5729a8adadf256221395d3912c09dda
KseniyaShulha <kseniyashulha90@gmail.com>
2026-09-25 16:11:27 +0200

    TOOLS-79 remove test.skip for TOOLS_19_logout

--- tests/api/TOOLS_19_logout.spec.ts
+++ tests/api/TOOLS_19_logout.spec.ts
@@ -4,7 +4,7 @@ import { getTokenFromJson } from "../../api/apiHelper";
 
 let token: any;
 
-test.skip("Logout", { tag: ["@api", "@regression", "@auth"] }, () => {
+test("Logout", { tag: ["@api", "@regression", "@auth"] }, () => {
   test.beforeEach(async () => {
     token = await getTokenFromJson("admin");
 
